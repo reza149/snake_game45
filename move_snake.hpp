f2441e59f5504eaad6e07242d225dc3624da1179
alireza <alirezakarami30780@gmail.com>
2023-01-06 13:06:37 +0330

    final move_snake class

--- move_snake.hpp
+++ move_snake.hpp
@@ -20,6 +20,85 @@ class move_snake
 
 };
 
+void move_snake::d_move(play_board * game)
+{
+    snake_body help;
+    snake_body help2;
+    int column = (*game).sb_arr[0].i_number - (*game).sb_arr[1].i_number;
+    int row = (*game).sb_arr[0].j_number - (*game).sb_arr[1].j_number;
+    switch(column)
+    {
+        case -1:
+        help.i_number = (*game).sb_arr[0].i_number;
+        help.j_number = (*game).sb_arr[0].j_number;
+        (*game).sb_arr[0].i_number = (*game).sb_arr[0].i_number;
+        (*game).sb_arr[0].j_number = (*game).sb_arr[0].j_number + 1;
+        for(int i = 1; i < (*game).sb_arr.size(); i++)
+        {
+            help2.i_number = (*game).sb_arr[i].i_number;
+            help2.j_number = (*game).sb_arr[i].j_number;
+            (*game).sb_arr[i].i_number = help.i_number;
+            (*game).sb_arr[i].j_number = help.j_number;
+            help.i_number = help2.i_number;
+            help.j_number = help2.j_number;
+        }
+
+        break;
+        ///////
+        case 1:
+        help.i_number = (*game).sb_arr[0].i_number;
+        help.j_number = (*game).sb_arr[0].j_number;
+        (*game).sb_arr[0].i_number = (*game).sb_arr[0].i_number;
+        (*game).sb_arr[0].j_number = (*game).sb_arr[0].j_number - 1;
+        for(int i = 1; i < (*game).sb_arr.size(); i++)
+        {
+            help2.i_number = (*game).sb_arr[i].i_number;
+            help2.j_number = (*game).sb_arr[i].j_number;
+            (*game).sb_arr[i].i_number = help.i_number;
+            (*game).sb_arr[i].j_number = help.j_number;
+            help.i_number = help2.i_number;
+            help.j_number = help2.j_number;
+        }
+
+        break;
+        ///////
+        case 0:
+        if(row == 1)
+        {
+            help.i_number = (*game).sb_arr[0].i_number;
+            help.j_number = (*game).sb_arr[0].j_number;
+            (*game).sb_arr[0].i_number = (*game).sb_arr[0].i_number + 1;
+            (*game).sb_arr[0].j_number = (*game).sb_arr[0].j_number;
+            for(int i = 1; i < (*game).sb_arr.size(); i++)
+            {
+                help2.i_number = (*game).sb_arr[i].i_number;
+                help2.j_number = (*game).sb_arr[i].j_number;
+                (*game).sb_arr[i].i_number = help.i_number;
+                (*game).sb_arr[i].j_number = help.j_number;
+                help.i_number = help2.i_number;
+                help.j_number = help2.j_number;
+            }
+        }
+        if(row == -1)
+        {
+            help.i_number = (*game).sb_arr[0].i_number;
+            help.j_number = (*game).sb_arr[0].j_number;
+            (*game).sb_arr[0].i_number = (*game).sb_arr[0].i_number - 1;
+            (*game).sb_arr[0].j_number = (*game).sb_arr[0].j_number;
+            for(int i = 1; i < (*game).sb_arr.size(); i++)
+            {
+                help2.i_number = (*game).sb_arr[i].i_number;
+                help2.j_number = (*game).sb_arr[i].j_number;
+                (*game).sb_arr[i].i_number = help.i_number;
+                (*game).sb_arr[i].j_number = help.j_number;
+                help.i_number = help2.i_number;
+                help.j_number = help2.j_number;
+            }
+        }
+        break;
+    }
+}
+
 void move_snake::a_move(play_board * game)
 {
     snake_body help;
@@ -29,10 +108,36 @@ void move_snake::a_move(play_board * game)
     switch(column)
     {
         case -1:
-        
+        help.i_number = (*game).sb_arr[0].i_number;
+        help.j_number = (*game).sb_arr[0].j_number;
+        (*game).sb_arr[0].i_number = (*game).sb_arr[0].i_number;
+        (*game).sb_arr[0].j_number = (*game).sb_arr[0].j_number - 1;
+        for(int i = 1; i < (*game).sb_arr.size(); i++)
+        {
+            help2.i_number = (*game).sb_arr[i].i_number;
+            help2.j_number = (*game).sb_arr[i].j_number;
+            (*game).sb_arr[i].i_number = help.i_number;
+            (*game).sb_arr[i].j_number = help.j_number;
+            help.i_number = help2.i_number;
+            help.j_number = help2.j_number;
+        }
+        break;
 
         case 1:
-        
+        help.i_number = (*game).sb_arr[0].i_number;
+        help.j_number = (*game).sb_arr[0].j_number;
+        (*game).sb_arr[0].i_number = (*game).sb_arr[0].i_number;
+        (*game).sb_arr[0].j_number = (*game).sb_arr[0].j_number + 1;
+        for(int i = 1; i < (*game).sb_arr.size(); i++)
+        {
+            help2.i_number = (*game).sb_arr[i].i_number;
+            help2.j_number = (*game).sb_arr[i].j_number;
+            (*game).sb_arr[i].i_number = help.i_number;
+            (*game).sb_arr[i].j_number = help.j_number;
+            help.i_number = help2.i_number;
+            help.j_number = help2.j_number;
+        }
+        break;
 
         case 0:
         if(row == 1)
@@ -49,7 +154,22 @@ void move_snake::a_move(play_board * game)
                 (*game).sb_arr[i].j_number = help.j_number;
                 help.i_number = help2.i_number;
                 help.j_number = help2.j_number;
-                cout << help2.i_number << endl;
+            }
+        }
+        if(row == -1)
+        {
+            help.i_number = (*game).sb_arr[0].i_number;
+            help.j_number = (*game).sb_arr[0].j_number;
+            (*game).sb_arr[0].i_number = (*game).sb_arr[0].i_number + 1;
+            (*game).sb_arr[0].j_number = (*game).sb_arr[0].j_number;
+            for(int i = 1; i < (*game).sb_arr.size(); i++)
+            {
+                help2.i_number = (*game).sb_arr[i].i_number;
+                help2.j_number = (*game).sb_arr[i].j_number;
+                (*game).sb_arr[i].i_number = help.i_number;
+                (*game).sb_arr[i].j_number = help.j_number;
+                help.i_number = help2.i_number;
+                help.j_number = help2.j_number;
             }
         }
 
@@ -79,6 +199,7 @@ void move_snake::w_move(play_board * game)
             help.i_number = help2.i_number;
             help.j_number = help2.j_number;
         }
+        break;
 
         case 1:
         help.i_number = (*game).sb_arr[0].i_number;
@@ -94,6 +215,7 @@ void move_snake::w_move(play_board * game)
             help.i_number = help2.i_number;
             help.j_number = help2.j_number;
         }
+        break;
 
         case 0:
         if(row == 1)
@@ -140,13 +262,14 @@ void move_snake::set_move(char move_char, play_board * game)
     {
         case 'a':
         a_move(game);
+        break;
 
         case 'w':
         w_move(game);
+        break;
 
-        case 's':
-
-
+        case 'd':
+        d_move(game);
         break;
     }
 }
